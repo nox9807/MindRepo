174e0a689c02e522babeb8c77bf2513c77ad05a9
hot666666 <hotcs6@naver.com>
2025-10-15 16:39:09 +0900

    feat: MM월 yyyy 형태 포맷 추가 - Date+ 파일  내, formattedMY_ko 구현 추가

--- MindRepo/Extension/Date+.swift
+++ MindRepo/Extension/Date+.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 extension Date {
-    /// Shared formatter: 2025년 10월 14일 형태
+    
+    // MARK: - Shared formatter: `2025년 10월 14일` 형태
+    
     fileprivate static let koYMDFormatter: DateFormatter = {
         let df = DateFormatter()
         df.locale = Locale(identifier: "ko_KR")
@@ -20,5 +22,20 @@ extension Date {
     /// `selectedDate.formattedYMD_ko`를 이용하는 포맷
     var formattedYMD_ko: String {
         Self.koYMDFormatter.string(from: self)
+    }
+    
+    // MARK: - Shared formatter: `10월 2025` 형태
+    
+    fileprivate static let koMYFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.locale = Locale(identifier: "ko_KR")
+        df.calendar = Calendar(identifier: .gregorian)
+        df.dateFormat = "MM월 yyyy"
+        return df
+    }()
+    
+    /// `selectedDate.formattedMY_ko`를 이용하는 포맷
+    var formattedMY_ko: String {
+        Self.koMYFormatter.string(from: self)
     }
 }
