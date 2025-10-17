52885a74baeb15ca30c224da94b551387f64cd53
chanyoungkm <53ch0905@naver.com>
2025-10-17 11:26:26 +0900

    fix: 파일 이름 변경
    - MoodStat 에서 MoodStatisticsView 로 변경

--- MindRepo/View/Main/MainView.swift
+++ MindRepo/View/Main/MainView.swift
@@ -39,7 +39,7 @@ struct MainView: View {
             case .editor:
                 DiaryEditorView()
             case .stats:
-                MoodStatView()
+                MoodStatisticsView()
         }
     }
     // MARK: -tabButton 기능
